5d2e443f1366834c16fecaa989565ffc9b664c91
simsim2898 <jehyun2898@gmail.com>
2024-07-04 13:01:21 +0900

    commit

--- Baekjoon/4Level/6_10813.py
+++ Baekjoon/4Level/6_10813.py
@@ -1,6 +1,7 @@
 n, m = map(int, input().split())
-a = 0
+box = [i for i in range(1, n + 1)]
 for _ in range(m):
-        i,j = map(int,input().split())
-        for a in range(i, j+1):
-            i = [a] 
+    i, j = map(int, input().split())
+    box[i-1], box[j-1] = box[j-1], box[i-1] 
+for box in box:
+    print(box, end=' ') 
