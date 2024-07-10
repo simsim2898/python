7aa6893f5feca42e17d7cf4b4ddf14f6f8e02c3d
simsim2898 <jehyun2898@gmail.com>
2024-07-10 21:29:38 +0900

    commit

---
+++ Baekjoon/7step/1_2738.py
@@ -0,0 +1,27 @@
+n, m = map(int, input().split())
+A = []
+B = []
+for i in range(n):
+    a = list(map(int,input().split()))
+    A.append(a)
+        
+for i in range(n):
+    b = list(map(int,input().split()))
+    B.append(b)
+    
+for i in range(n):
+    for j in range(m):
+        result = A[i][j] + B[i][j]
+        print(result,end=' ')
+    print()
+    
+# list를 합쳐버리기
+# n, m = map(int, input().split())
+# A = [list(map(int, input().split())) for _ in range(n)]
+# B = [list(map(int, input().split())) for _ in range(n)]
+
+#for i in range(n):
+#    for j in range(m):
+#        result = A[i][j] + B[i][j]
+#        print(result, end=' ')
+#    print()
