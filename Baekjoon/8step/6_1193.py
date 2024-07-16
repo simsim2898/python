3cea1f9a6ea112919f40cdb3d0a3bed782a2f2b9
simsim2898 <jehyun2898@gmail.com>
2024-07-16 19:15:43 +0900

    commit

--- Baekjoon/8step/6_1193.py
+++ Baekjoon/8step/6_1193.py
@@ -1,16 +1,16 @@
 n = int(input())
 a = 1
 
-while n > 0:
+while n > a:
     n -= a
     a += 1
+
 if a % 2 == 0:
-    up = n
-    down = a - n + 1
+    numerator = n
+    denominator = a - n + 1
 else:
-    up = a - n + 1
-    down = n
-print(up, '/', down, sep="")
+    numerator = a - n + 1
+    denominator = n
 
+print(f"{numerator}/{denominator}")
 
-## 아 몰라 나중에해ㅑ
